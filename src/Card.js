6cc81260397e2a279fe9d34b7549fd353730e82d
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-09-29 15:07:30 +0530

    Initial Commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="Robot pictures" src="https://robohash.org/test" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>Janedoe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
