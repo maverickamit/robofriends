06d7e9c9e8660b66731447fa5fde847191c0f93c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-09-29 15:34:47 +0530

    added styling to card component

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div>
+    <div className="bg-light-green dib tc br3 pa3 ma2">
       <img alt="Robot pictures" src="https://robohash.org/test?size=150x150" />
       <div>
         <h2>Jane Doe</h2>
