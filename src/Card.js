82dcd11da9a2ea4357db99cf9bd191618026e69a
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-09-29 15:30:02 +0530

    added tachyons

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   return (
     <div>
-      <img alt="Robot pictures" src="https://robohash.org/test" />
+      <img alt="Robot pictures" src="https://robohash.org/test?size=150x150" />
       <div>
         <h2>Jane Doe</h2>
         <p>Janedoe@gmail.com</p>
