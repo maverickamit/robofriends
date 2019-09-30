73a6d436341fe5c72ac287a96ed26a9352821380
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-09-30 12:17:48 +0530

    modified App component to class constructor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
-const App = () => {
-  return (
-    <div>
-      <h1 className="tc"> RoboFriends </h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    robots: robots,
+    searchField: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="tc"> RoboFriends </h1>
+        <SearchBox />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
