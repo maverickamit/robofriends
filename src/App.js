a9816a135ef87eb8b5290719a1660ed5cee38e4a
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-09-30 11:49:33 +0530

    Added Searchbox component and app title.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
 const App = () => {
   return (
     <div>
+      <h1 className="tc"> RoboFriends </h1>
+      <SearchBox />
       <CardList robots={robots} />
     </div>
   );
