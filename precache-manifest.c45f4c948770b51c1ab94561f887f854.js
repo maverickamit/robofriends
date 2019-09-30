9897ce84981bc56b8c64d0ec27103433985cb2a7
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-09-30 19:13:07 +0530

    Updates

---
+++ precache-manifest.c45f4c948770b51c1ab94561f887f854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f536920ea615f6c2a47f21c88a89c8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ab834e1a52becdace82a",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3d39a3c1ac041efaa9b9",
+    "url": "/robofriends/static/css/main.f4427645.chunk.css"
+  },
+  {
+    "revision": "ab834e1a52becdace82a",
+    "url": "/robofriends/static/js/2.c4d68144.chunk.js"
+  },
+  {
+    "revision": "3d39a3c1ac041efaa9b9",
+    "url": "/robofriends/static/js/main.6025808b.chunk.js"
+  },
+  {
+    "revision": "ef7ed2c3e67d53841831",
+    "url": "/robofriends/static/js/runtime-main.ed74653d.js"
+  }
+]);
